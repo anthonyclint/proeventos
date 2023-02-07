f7016b5e08aa0343c1dbe965718846c891308f22
unknown <anthony.pastorello@modalgr.com.br>
2023-02-07 15:57:35 -0300

    Seção 3: Introdução ao EF Core.

--- Backend/src/PROEVENTOS.API/Controllers/EventoController.cs
+++ Backend/src/PROEVENTOS.API/Controllers/EventoController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-
+using PROEVENTOS.API.Data;
 using PROEVENTOS.API.Models;
 
 namespace PROEVENTOS.API.Controllers;
@@ -7,42 +7,24 @@ namespace PROEVENTOS.API.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
-{
-    public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento(){
-                EventoId = 1,
-                Local = "Belo Horizonte",
-                Tema = "Angular e .NET Core",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Local = "Santos",
-                Tema = "Angular e .NET Core",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemUrl = "foto2.png"
-            }
-    };
-    
-    public EventoController()
+{    
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
-
+        _context = context;
     }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-        return _evento;
+        return _context.Eventos;
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
+    public Evento GetById(int id)
     {
-        return _evento.Where(evento => evento.EventoId == id);
+        return _context.Eventos.FirstOrDefault(
+            evento => evento.EventoId == id
+        );
     }
 }
---
+++ Backend/src/PROEVENTOS.API/Data/Migrations/20230207180630_Initial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PROEVENTOS.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Local = table.Column<string>(type: "longtext", nullable: false),
+                    DataEvento = table.Column<string>(type: "longtext", nullable: false),
+                    Tema = table.Column<string>(type: "longtext", nullable: false),
+                    QtdPessoas = table.Column<int>(type: "int", nullable: false),
+                    Lote = table.Column<string>(type: "longtext", nullable: false),
+                    ImagemUrl = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
